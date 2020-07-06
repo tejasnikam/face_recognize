710896419d2e2cdeda48b27945a72ec3cfa839ba
tejas nikam <tnikam2@gmail.com>
2020-07-06 16:21:10 +0530

    Add files via upload

---
+++ face_index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <script src="jquery_min.js"></script>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <script defer src="face-api.js"></script>
+  <script defer src="script_face_api.js"></script>
+  <title>Face Recognition</title>
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      width: 100vw;
+      height: 100vh;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column
+    }
+
+    canvas {
+      position: absolute;
+      top: 48px;
+      left:326px;
+    }
+  </style>
+</head>
+<body>
+ <!-- <input type="file" id="imageUpload"> -->
+  <video id="video" width="720" height="560" autoplay muted></video>  
+
+  <!--<input type="button" id="ajaxCall"> ->
+</body>
+</html>
